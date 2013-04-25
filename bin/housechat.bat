9af0b8b6e7fa31fb31b5e682935384eda76aa7a9
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2013-04-25 10:02:33 +0100

    Really do not ignore bin

---
+++ bin/housechat.bat
@@ -0,0 +1,4 @@
+cd ..
+java -cp "lib/*;bin" -Djava.library.path=bin net.geekgrandad.apps.HouseChat
+cd bin
+
---
+++ bin/housecontrol.sh
@@ -0,0 +1,4 @@
+cd ..
+java -cp "lib/*:bin" -Djava.library.path=bin net.geekgrandad.HouseControl
+cd bin
+
