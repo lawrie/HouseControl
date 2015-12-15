54a5dd15ce190ddbc1c0fd50f4f916e85c93e36c
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2015-12-15 07:22:12 +0000

    Service script

--- bin/housecontrol.sh
+++ bin/housecontrol.sh
@@ -2,5 +2,5 @@ cd ..
 rm nohup.out
 mv log/app.log log/app.old
 nohup java -Djava.library.path=/usr/lib/jni -cp "/usr/share/java/RXTXcomm.jar:lib/*:bin" net.geekgrandad.HouseControl $1 &
+[ "$#" -eq "2" ] && echo $! >$2
 cd bin
-
