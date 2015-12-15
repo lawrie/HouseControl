cc307e4032c6cc5c7a945538425b55607c4ba7b2
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2015-12-15 10:08:48 +0000

    Removed unused entries from config

--- src/housecontrol.sh
+++ src/housecontrol.sh
@@ -1,7 +1,6 @@
 cd ..
 rm nohup.out
 mv log/app.log log/app.old
-export DISPLAY=:0
 nohup java -Djava.library.path=/usr/lib/jni -cp "/usr/share/java/RXTXcomm.jar:lib/*:bin" net.geekgrandad.HouseControl $1 &
 [ "$#" -eq "2" ] && echo $! >$2
 cd bin
