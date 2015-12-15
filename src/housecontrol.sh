7c7ab1293b3390dbdc238f124263703ed3a8d405
Lawrie Griffiths <lawrie.griffiths@ntlworld.com>
2015-12-15 09:14:50 +0000

    Run as non-root user

--- src/housecontrol.sh
+++ src/housecontrol.sh
@@ -1,6 +1,7 @@
 cd ..
 rm nohup.out
 mv log/app.log log/app.old
+export DISPLAY=:0
 nohup java -Djava.library.path=/usr/lib/jni -cp "/usr/share/java/RXTXcomm.jar:lib/*:bin" net.geekgrandad.HouseControl $1 &
 [ "$#" -eq "2" ] && echo $! >$2
 cd bin
